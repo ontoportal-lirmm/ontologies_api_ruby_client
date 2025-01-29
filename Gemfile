e8785fe23ff9787133481373e561b59f2adade05
Bilel KIHAL <kb_kihal@esi.dz>
2025-01-29 15:50:33 +0100

    add rails cache to tests envirenement

--- Gemfile
+++ Gemfile
@@ -6,3 +6,6 @@ gem 'rake'
 gem 'pry'
 gem 'test-unit'
 gem 'webmock', require: false
+# Add these lines to your Gemfile
+gem 'rails'
+gem 'activesupport'
--- test/test_case.rb
+++ test/test_case.rb
@@ -1,9 +1,18 @@
 require 'test-unit'
+require 'active_support'
+require 'active_support/cache'
 require_relative '../lib/ontologies_api_client'
 require_relative '../config/config'
 require 'webmock'
 
 WebMock.allow_net_connect!
+
+module Rails
+  def self.cache
+    @cache ||= ActiveSupport::Cache::MemoryStore.new
+  end
+end
+
 module LinkedData
   module Client
     class TestCase < Test::Unit::TestCase
