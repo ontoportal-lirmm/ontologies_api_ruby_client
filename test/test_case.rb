ba4e729462c8887d744ca1cbccfdd96da5947328
Bilel KIHAL <kb_kihal@esi.dz>
2025-01-29 17:55:22 +0100

    initialize logger and update activesupport version

--- test/test_case.rb
+++ test/test_case.rb
@@ -1,5 +1,7 @@
 require 'test-unit'
+require 'logger'
 require 'active_support'
+require 'active_support/logger'
 require 'active_support/cache'
 require_relative '../lib/ontologies_api_client'
 require_relative '../config/config'
@@ -7,13 +9,12 @@ require 'webmock'
 
 WebMock.allow_net_connect!
 
-# Set up a cache for testing
+Logger = ::Logger unless defined?(Logger)
 CACHE = ActiveSupport::Cache::MemoryStore.new
 
 module LinkedData
   module Client
     class TestCase < Test::Unit::TestCase
-      # You can use CACHE in your tests if needed
     end
   end
 end
